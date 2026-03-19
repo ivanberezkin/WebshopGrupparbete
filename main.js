1a9474833888d01479ea2dee31fb2a40668b2fa0
Viktor Nikander <vikvil91@gmail.com>
2026-03-19 11:28:09 +0100

    add getFakeStore()

--- main.js
+++ main.js
@@ -3,7 +3,26 @@ function getFakeStore() {
     .then((response) => response.json())
     .then((data) => {
       data.forEach(element => {
-        console.log(element);
+                    document.getElementById("addElementsHere").innerHTML += `
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="${element.image}" alt="${element.title}" />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">${element.title}</h5>
+                                    <!-- Product price-->
+                                    $${element.price.toFixed(2)}
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Buy Now</a></div>
+                            </div>
+                        </div>
+                     </div>`;
       });
     });
 }
